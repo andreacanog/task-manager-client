e1be34c01f772ab17160d56ab389a51524b9de1f
Andrea Cano <andreacanogisbert@gmail.com>
2026-04-08 16:34:56 -0700

    Add description field to GET_BOARD query

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -16,6 +16,7 @@ const GET_BOARD = gql`
                 tasks {
                     id
                     title
+                    description
                     completed
                 }
             }
